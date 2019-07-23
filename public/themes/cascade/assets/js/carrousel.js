90f853318e2437d4391c0e560c6f00a0f2b0f245
pchantois <pascalchantois@gmail.com>
2019-07-23 10:04:27 +0200

    PC : update du carrousel

--- public/themes/cascade/assets/js/carrousel.js
+++ public/themes/cascade/assets/js/carrousel.js
@@ -1,4 +1,13 @@
 
 $(document).ready(function(){
-  $('.carrousel').slick();
+  $('.carrousel').slick({
+      slideToShow: 1,
+      slideToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      adaptiveHeight: true
+  });
 });
